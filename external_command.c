e7db980ccd2ac25b437cca9583db942886550c59
atarsi <aletarsi97@gmail.com>
2022-10-30 18:38:36 +0100

    norminette

--- external_command.c
+++ external_command.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   external_command.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ebondi <ebondi@student.42roma.it>          +#+  +:+       +#+        */
+/*   By: atarsi <atarsi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 11:36:59 by atarsi            #+#    #+#             */
-/*   Updated: 2022/10/29 18:23:44 by ebondi           ###   ########.fr       */
+/*   Updated: 2022/10/30 18:35:42 by atarsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
 int	ft_find_path(t_mini *mini, char **cmd)
 {
 	char	*paths;
@@ -18,26 +19,28 @@ int	ft_find_path(t_mini *mini, char **cmd)
 	int		i;
 
 	paths = ft_get_env_var(mini, "PATH");
-	pos_path = ft_smart_split(paths, ":");
+	pos_path = ft_smart_split(paths, ':');
 	i = 0;
-	while (!ft_isspace(cmd[0][i])
+	while (!ft_isspace(cmd[0][i]))
 		i++;
 	printf("%s", cmd[0]);
+/*
 	i = 0;
-	// while (pos_path[i] != NULL)
-	// {
-		
-	// 	if (access())	
-	// }
+	while (pos_path[i] != NULL)
+	{
+		if (access())	
+	}
+*/
 	return (0);
 }
 
 int	ft_ext_cmd(char **cmd, t_mini *mini)
 {
-	// if (strchr())
-	// //gestire ./
-	// else
-		ft_find_path(mini, cmd);
-	
+/*
+	if (strchr())
+		gestire ./
+	else
+*/
+	ft_find_path(mini, cmd);
 	return (0);
 }
